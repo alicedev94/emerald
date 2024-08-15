a35c97ed6577294d3159fc062e3e9aa74ddd5652
alicedev94 <alicedev94@gmail.com>
2024-08-15 16:56:39 -0400

    A simple reusable component library for Vue.

--- core/src/App.vue
+++ core/src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import Header from '@/components/Header.vue';
-import Tray from '@/components/Tray.vue';
+
+// A simple reusable component library for Vue
 
 </script>
 
