6244dca6a3dfb569e4a310b200e780c0d7e23607
alicedev94 <alicedev94@gmail.com>
2024-10-15 23:38:52 -0400

    css test init

--- README.md
+++
@@ -1,3 +0,0 @@
-A simple reusable component library for Vue.
-
-Author(@): alice :3
--- src/components/SignIn.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import Tray from './TrayC.vue';
-</script>
-
-<template>
-    <div>
-        <Tray title="INICIAR SESIÓN" />
-    </div>
-</template>
